25bf3a4951795279a1abb4985e72f2f37a5e8df6
Nathan Friend <hello@nathanfriend.io>
2018-05-24 10:05:20 -0300

    adding polyfill for NodeList.forEach

---
+++ material/login/resources/js/polyfill/nodelist-foreach.js
@@ -0,0 +1,11 @@
+// Taken from https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach
+// Allows for document.querySelectorAll('.selector').forEach(...)
+
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function (callback, thisArg) {
+        thisArg = thisArg || window;
+        for (var i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
+}
